d6c23d5a25da213532d4e0daca543569ce01904e
TruongTrongTien <trongtientruong2003@gmail.com>
2024-03-14 15:13:51 +0700

    Phase2/TienTT: Add CV Matcher

--- pages/4_match.py
+++ pages/4_match.py
@@ -1,25 +1,55 @@
 import streamlit as st
-from utils.match_utils import get_jd, get_cv, get_info_jd, get_info_cv, match, prettify_match
+from utils.match_utils import get_jd, get_cv, get_info_jd, get_info_cv, get_text_jd, get_text_cv, slice_id, match, prettify_match
 
 st.set_page_config(page_title="CV Matcher")
 
+st.header("CV Matcher", divider="rainbow")
+
 jd_option = st.selectbox(
-    "Select a Job Description in the Database",
+    "Select a Job Description from the Database",
     get_info_jd(get_jd()),
     index=None,
     placeholder="Select a Job Description"
 )
 
+if jd_option is not None:
+    with st.expander("Preview"):
+        id_jd = slice_id(jd_option)
+        st.write(get_text_jd(get_jd(), id_jd))
+
 cv_option = st.selectbox(
-    "Select CV(s) in the Database",
+    "Select CV(s) from the Database",
     get_info_cv(get_cv()),
     index=None,
     placeholder="Select one or more CVs"
 )
 
-if jd_option is not None and cv_option is not None:
-    st.text((match(jd_option, cv_option)))
-else:
-    st.info("Result")
+if cv_option is not None:
+    id_cv = slice_id(cv_option)
 
-# overall_match, skills_match, experience_match, required_skills, candidate_skills, required_experience, candidate_experience = prettify_match(match(jd_option, cv_option))
+if jd_option is not None and cv_option is not None:
+    match_result = match(id_jd, id_cv)
+    st.success("Matching Completed")
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.metric("Overall Match Percentage", f"{match_result.get("result").get("Overall Match Percentage:")}")
+    with col2:
+        st.metric("Skills Match Percentage:", f"{match_result.get("result").get("Skills Match").get("Match Percentage")}")
+    with col3:
+        st.metric("Experience Match Percentage:", f"{match_result.get("result").get("Experience Match").get("Match Percentage")}")
+
+    st.subheader("Skills Match")
+    st.write("• Required Skills")
+    st.write(match_result.get("result").get("Skills Match").get("Required Skills"))
+    st.write("• Candidate Skills") 
+    st.write(match_result.get("result").get("Skills Match").get("Candidate Skills"))
+    
+    st.subheader("Experience Match")
+    st.write("• Required Experience")
+    st.write(match_result.get("result").get("Experience Match").get("Required Experience"))
+    st.write("• Candidate Experience")
+    st.write(match_result.get("result").get("Experience Match").get("Candidate Experience"))
+    
+    st.subheader("Explanation")
+    st.write(match_result.get("result").get("Explanation"))
--- utils/match_utils.py
+++ utils/match_utils.py
@@ -1,18 +1,24 @@
 import requests
 import json
 import re
+import os
+from dotenv import load_dotenv
 
-def get_cv():
+load_dotenv()
+os.environ['BACKEND_URL_API'] = os.getenv('BACKEND_URL_API')
+BACKEND_URL_API = os.environ.get("BACKEND_URL_API")
+
+def get_jd():
     # Define the URL
-    url = "https://millmin-fsa-project-cv.hf.space/modules/crud_cvs_router/"
+    url = f"{BACKEND_URL_API}/modules/crud_jds_router/"
     # Send a GET request
     response = requests.get(url)
     # Print the response content
     return json.loads(response.content.decode("utf-8"))
 
-def get_jd():
+def get_cv():
     # Define the URL
-    url = "https://millmin-fsa-project-cv.hf.space/modules/crud_jds_router/"
+    url = f"{BACKEND_URL_API}/modules/crud_cvs_router/"
     # Send a GET request
     response = requests.get(url)
     # Print the response content
@@ -26,12 +32,24 @@ def get_info_cv(cv_list):
     info_cv_list = [f"{cv.get("name_candidate")} - {cv.get("apply_position")} [{cv.get("id_cv")}]" for cv in cv_list]
     return info_cv_list
 
-def match(id_jd, id_cv):
-    id_jd_0 = re.search(r'\[(.*?)\]', id_jd).group(1)
-    id_cv_0 = re.search(r'\[(.*?)\]', id_cv).group(1)
+def get_text_jd(jd_list, id_jd):
+    for jd in jd_list:
+        if jd.get("id_jd") == id_jd:
+            return jd.get("jd_text")
+    return None
 
-    url = "https://millmin-fsa-project-cv.hf.space/modules/cvmatching/matching/"
-    completed_url = f"{url.rstrip("/")}?id_jd={id_jd_0}&id_cv={id_cv_0}"
+def get_text_cv(cv_list, id_cv):
+    for jd in cv_list:
+        if jd.get("id_cv") == id_cv:
+            return jd.get("cv_content")
+    return None
+
+def slice_id(info: str):
+    return re.search(r'\[(.*?)\]', info).group(1)
+
+def match(id_jd, id_cv):
+    url = f"{BACKEND_URL_API}/modules/cvmatching/matching/"
+    completed_url = f"{url.rstrip("/")}?id_jd={id_jd}&id_cv={id_cv}"
     response = requests.post(completed_url)
     return json.loads(response.content.decode("utf-8"))
 
@@ -40,7 +58,8 @@ def prettify_match(match):
     skills_match = match.get("Skills Match").get("Match Percentage")
     experience_match = match.get("Experience Match").get("Match Percentage")
     required_skills = match.get("Skills Match").get("Required Skills")
-    candidate_skills = match.get("Skills Match").get("Camdidate  Skills")
+    candidate_skills = match.get("Skills Match").get("Candidate  Skills")
     required_experience = match.get("Experience Match").get("Required Experience")
     candidate_experience = match.get("Experience Match").get("Candidate Experience")
-    return overall_match, skills_match, experience_match, required_skills, candidate_skills, required_experience, candidate_experience
+    explanation = match.get("Explanation")
+    return overall_match, skills_match, experience_match, required_skills, candidate_skills, required_experience, candidate_experience, explanation
